8cbadd7a0206c076ab91f47e4a85383cb2639e7f
David Ghazi <davidaghazi@gmail.com>
2020-08-05 23:57:39 -0400

    add routes + react mdl

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { Layout, Header, Navigation, Drawer, Content } from 'react-mdl';
+
+import Main from './components/main'
+import { Link } from 'react-router-dom';
+
+class App extends Component {
+  render() {
+    return (
+<div style={{height: '300px', position: 'relative'}}>
+    <Layout style={{background: 'url(http://www.getmdl.io/assets/demos/transparent.jpg) center / cover'}}>
+        <Header transparent title="My Portfolio" style={{color: 'white'}}>
+            <Navigation>
+                <Link to="/about">About Me</Link>
+                <Link to="/projects">Projects</Link>
+                <Link to="/resume">Resume</Link>
+                <Link to="/contact">Contact</Link>
+            </Navigation>
+        </Header>
+        <Drawer title="My Portfolio">
+            <Navigation>
+                <Link to="/about">About Me</Link>
+                <Link to="/projects">Projects</Link>
+                <Link to="/resume">Resume</Link>
+                <Link to="/contact">Contact</Link>
+            </Navigation>
+        </Drawer>
+        <Content>
+            <div className="page-content"></div>
+            <Main />
+        </Content> />
+    </Layout>
+</div>
+    );
+  }
 }
 
 export default App;
