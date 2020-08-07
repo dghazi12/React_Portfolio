69de89e74ce2b28c8f3250e84b5d15656dbb1594
David Ghazi <davidaghazi@gmail.com>
2020-08-06 23:26:18 -0400

    updated homepage of portfolio

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -9,9 +9,9 @@ import { Link } from 'react-router-dom';
 class App extends Component {
   render() {
     return (
-<div style={{height: '300px', position: 'relative'}}>
-    <Layout style={{background: 'url(http://www.getmdl.io/assets/demos/transparent.jpg) center / cover'}}>
-        <Header transparent title="My Portfolio" style={{color: 'white'}}>
+    <div style={{height: '720px', position: 'relative'}}>
+    <Layout fixedHeader>
+    <Header className="header-color" title={<span><span style={{ color: '#ddd' }}></span><strong>My Portfolio</strong></span>}>
             <Navigation>
                 <Link to="/about">About Me</Link>
                 <Link to="/projects">Projects</Link>
@@ -30,11 +30,43 @@ class App extends Component {
         <Content>
             <div className="page-content"></div>
             <Main />
-        </Content> />
+        </Content>
     </Layout>
-</div>
+    </div>
     );
   }
 }
+
+
+// class App extends Component {
+//   render() {
+//     return (
+//     <div style={{height: '720px', position: 'relative'}}>
+//     <Layout style={{background: 'url(https://www.ibm.com/blogs/internet-of-things/wp-content/uploads/2019/02/leadspace_clairepenny.jpg) center / cover'}}>
+//         <Header transparent title="My Portfolio" style={{color: 'white'}}>
+//             <Navigation>
+//                 <Link to="/about">About Me</Link>
+//                 <Link to="/projects">Projects</Link>
+//                 <Link to="/resume">Resume</Link>
+//                 <Link to="/contact">Contact</Link>
+//             </Navigation>
+//         </Header>
+//         <Drawer title="My Portfolio">
+//             <Navigation>
+//                 <Link to="/about">About Me</Link>
+//                 <Link to="/projects">Projects</Link>
+//                 <Link to="/resume">Resume</Link>
+//                 <Link to="/contact">Contact</Link>
+//             </Navigation>
+//         </Drawer>
+//         <Content>
+//             <div className="page-content"></div>
+//             <Main />
+//         </Content> />
+//     </Layout>
+//     </div>
+//     );
+//   }
+// }
 
 export default App;
--- react-portfolio/src/components/homepage.js
+++ react-portfolio/src/components/homepage.js
@@ -1,9 +1,39 @@
 import React, { Component } from 'react';
+import { Grid, Cell } from 'react-mdl';
 
 class Homepage extends Component {
     render() {
         return(
-            <div><h1>Homepage Page  </h1></div>
+            <div style={{width: '100%', margin: 'auto'}}>
+                <Grid className="Home-grid">
+                    <Cell col={12}>
+                        <img 
+                            src="https://cdn3.iconfinder.com/data/icons/complete-set-icons/512/skype2512x512.png"
+                            alt="Profile Picture"
+                            className="profile-img"
+                        />
+
+                        <div className="social-links">
+                            
+                            <a href="http://google.ca" rel="noopener noreferer" target="_blank">
+                                <i className="fa fa-linkedin-square" aria-hidden="true" />
+                            </a>
+
+                            <a href="http://google.ca" rel="noopener noreferer" target="_blank">
+                                <i className="fa fa-github-square" aria-hidden="true" />
+                            </a>
+
+                        </div>
+
+                        <div className="banner-text">
+                            <h1>Full Stack Web Developer</h1>
+                            
+                            <hr />
+
+                        </div>
+                    </Cell>
+                </Grid>
+            </div>
         )
     }
 }
