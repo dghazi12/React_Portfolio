cc9a775d9243b5485d262b1ee16a50e2d78fd473
David Ghazi <davidaghazi@gmail.com>
2020-12-15 21:57:29 -0500

    font styling + pages complete

--- react-portfolio/src/components/ContactForm.js
+++ react-portfolio/src/components/ContactForm.js
@@ -24,16 +24,16 @@ export default function ContactUs() {
             <form onSubmit={sendEmail}>
                     <div className="row pt-5 mx-auto">
                         <div className="col-8 form-group mx-auto">
-                            <input type="text" className="form-control name" cols="30" placeholder="Name" name="name"/>
+                            <input type="text" className="form-control name" cols="30" placeholder=" Name" name="name"/>
                         </div>
                         <div className="col-8 form-group pt-2 mx-auto">
-                            <input type="email" className="form-control name" cols="30" placeholder="Email Address" name="email"/>
+                            <input type="email" className="form-control name" cols="30" placeholder=" Email Address" name="email"/>
                         </div>
                         <div className="col-8 form-group pt-2 mx-auto">
-                            <input type="text" className="form-control name" cols="30"placeholder="Subject" name="subject"/>
+                            <input type="text" className="form-control name" cols="30"placeholder=" Subject" name="subject"/>
                         </div>
                         <div className="col-8 form-group pt-2 mx-auto">
-                            <textarea className="form-control message" cols="30" placeholder="Your message" name="message"></textarea>
+                            <textarea className="form-control message" cols="30" placeholder=" Message" name="message"></textarea>
                         </div>
                         <Button variant="outlined" type="submit" value="Send Message">
                             Send Message
